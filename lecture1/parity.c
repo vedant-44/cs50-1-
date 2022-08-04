54a2cf04fba87937a214acb20b058efdb1d3646f
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2022-08-04 05:41:25 +0000

    Thu, Aug 4, 2022, 5:41 AM +00:00

--- lecture1/parity.c
+++ lecture1/parity.c
@@ -2,5 +2,11 @@
 #include<stdio.h>
 int main(void)
 {
-    
+  int n=  get_int("which number do you want to check?");
+  if(%i % 2==0,n)
+  {
+    printf("even");
+    else
+    printf("odd");
+  }
 }
