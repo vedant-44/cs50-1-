7febe0357fa8096ec6c3bfe12f0ecd778461d253
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2022-08-04 15:50:47 +0000

    Thu, Aug 4, 2022, 3:50 PM +00:00

--- lecture1/calculator.c
+++ lecture1/calculator.c
@@ -2,8 +2,8 @@
 #include<stdio.h>
 int main(void)
 {
- char req= get_char("which operation do you want?");
- if (req==m)
+
+ 
  int x=  get_int("x:");
  int y=  get_int("y:");
   printf("ans:%i\n",x*y);
