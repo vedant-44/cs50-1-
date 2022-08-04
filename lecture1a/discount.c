a3fbe4603d1fad2dd58b54718ee5e416e520ca99
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2022-08-04 16:31:56 +0000

    Thu, Aug 4, 2022, 4:31 PM +00:00

--- lecture1a/discount.c
+++ lecture1a/discount.c
@@ -2,9 +2,9 @@
 #include<cs50.h>
 int main(void)
 {
-float original price=get_float("Original price:");
+float original =get_float("Original price:");
 int sale=get_int("sale:");
-float final_ price = original price*(100-sale)/100;
-printf("%2f",final price);
+float final  = original*(100-sale)/100;
+printf("%2f",final );
 
 }
