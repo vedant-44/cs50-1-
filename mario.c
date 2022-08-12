d98dd2af18b3dc8332ae258f7bc092f665c7b9b1
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2022-08-12 14:51:46 +0000

    ddd, Aug 8/12/2022, YYYY, 2:51 A +0

--- mario.c
+++ mario.c
@@ -7,4 +7,23 @@ do
 {
     n = get_int("height:");
 }
+while (n<1||n>8);
+int y=0;
+int x=n;
+for(int i=0;i<=n;i++)
+{
+    for(int j=y;j<n;j++)
+    {
+        printf(" ");
+    }
+    for(int k=x;k<n;k++)
+    {
+        printf("#");
+    }
+    y=y+1;
+    x=x-1;
+    printf("\n");
+    
+
+}
 }
