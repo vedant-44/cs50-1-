a5a123a088c5b58388fcf5c13d53535ab075faca
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2022-08-12 14:45:48 +0000

    ddd, Aug 8/12/2022, YYYY, 2:45 A +0

---
+++ mario.c
@@ -0,0 +1,10 @@
+#include<cs50.h>
+#include<stdio.h>
+int main(void)
+{
+    int n;
+do
+{
+    n = get_int("height:");
+}
+}
--- pset1/hello.c
+++
@@ -1,8 +0,0 @@
-# include<stdio.h>
-#include<cs50.h>
-int main(void)
-{
-   int age = get_int ("how old are you?");
-   printf("i am also, %i \n",age);
-   printf("we both are old\n");
-}
