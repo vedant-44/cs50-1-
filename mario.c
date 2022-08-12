d29c5ac6434c47c1f13c3a7d6b002349048282cc
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2022-08-12 05:40:45 +0000

    ddd, Aug 8/12/2022, YYYY, 5:40 A +0

---
+++ mario.c
@@ -0,0 +1,17 @@
+#include<cs50.h>
+#include<stdio.h>
+int main(void)
+{
+    int n = get_int("height:");
+
+int x =0;
+for(int i=0;i<n;i++)
+{
+    for(int j=x;j<n;j=j+1)
+    {
+        printf("#");
+    }
+x=x+1;
+printf("\n");
+}
+}
