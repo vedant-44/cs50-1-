d85c4aebd7c6b17df4e761b459a4e0ce19469b73
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2022-08-14 04:53:46 +0000

    ddd, Aug 8/14/2022, YYYY, 4:53 A +0

--- arrays.c
+++ arrays.c
@@ -3,12 +3,13 @@
 int main(void)
 {
     int n= get_int("how many scores?");
-    
-    for(i=0;i<n;i++)
+
+    for(int i=0;i<n;i++)
     {
-        int scores [i];
+        int scores [n];
         scores[i]=get_int("score:");
 
 
     }
+
 }
