4d85145fa2d1782742469803066132858135e34e
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2022-08-14 04:20:45 +0000

    ddd, Aug 8/14/2022, YYYY, 4:20 A +0

--- arrays.c
+++ arrays.c
@@ -2,8 +2,10 @@
 #include<stdio.h>
 int main(void)
 {
-    int scores[2];
-    scores[0]=45;
-    scores[1]=57;
-    printf("average:%i\n",scores[0]+scores[1]);
+    for(i=0;i<n;i++)
+    {
+        int scores [i];
+
+
+    }
 }
