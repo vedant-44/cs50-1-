960f54a1d876a3c36084518b2b8823c6740f3440
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2022-08-25 05:45:38 +0000

    ddd, Aug 8/25/2022, YYYY, 5:45 A +0

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -18,10 +18,12 @@ int main(void)
     int score1 = compute_score(word1);
     int score2 = compute_score(word2);
 
+
+
     // TODO: Print the winner
 }
 
 int compute_score(string word)
 {
-    
+   
 }
