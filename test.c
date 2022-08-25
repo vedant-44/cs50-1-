199d9ddda06a7050041c4d8175bb38d5457ffffd
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2022-08-25 07:22:06 +0000

    ddd, Aug 8/25/2022, YYYY, 7:22 A +0

--- test.c
+++ test.c
@@ -1,8 +1,20 @@
 #include<stdio.h>
 #include<cs50.h>
+#include<string.h>
 int main()
 {
-    string word1 = get_string ("word:");
-     int x =  word1[0];
+    string word = get_string ("word:");
+     int l = strlen(word);
+   int i=0;
+   while(i<l)
+   {
+    if(islower(word))
+    {
+        int x = word[i] - 97;
+
+    }
+
      printf("%i\n",x);
+
+   }
 }
