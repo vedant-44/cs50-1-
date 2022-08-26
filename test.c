1cb3a15da92aed3a4cfbf850740cddb101d822e0
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2022-08-26 04:33:26 +0000

    ddd, Aug 8/26/2022, YYYY, 4:33 A +0

--- test.c
+++ test.c
@@ -5,6 +5,7 @@
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 int main()
 {
+
     string word = get_string ("word:");
      int l = strlen(word);
    int i=0;
@@ -16,8 +17,8 @@ int main()
 
         i++;
 
-        int y = POINTS[x]
-        
+        int y = POINTS[x];
+
 
 
 
@@ -26,6 +27,7 @@ int main()
 
 
    }
+   printf("%i\n",y);
 
 
 }
