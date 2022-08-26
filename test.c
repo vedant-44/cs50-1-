d374d1bae05b1c0547c0a795e8ceb7f28d657827
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2022-08-26 04:46:34 +0000

    ddd, Aug 8/26/2022, YYYY, 4:46 A +0

--- test.c
+++ test.c
@@ -5,8 +5,16 @@
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 int main()
 {
+string word = get_string ("word:");
 
-    string word = get_string ("word:");
+
+
+}
+
+
+int scores (string word)
+{
+
      int l = strlen(word);
    int i=0;
    while(i<l)
@@ -22,12 +30,4 @@ int main()
 
    }
 
-
-
 }
-
-
-
-
-
-
