87bc4028142ded6f807f45722dca495e2cfedaeb
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2022-09-15 06:28:07 +0000

    ddd, Sep 9/15/2022, YYYY, 6:28 A +0

--- test.c
+++ test.c
@@ -1,7 +1,11 @@
 #include<cs50.h>
 #include<stdio.h>
+#include<string.h>
+#include<ctype.h>
 int main (void)
 {
     string word = get_string("word:");
-    
+    int l = strlen(word);
+    printf("%i\n",l);
+
 }
