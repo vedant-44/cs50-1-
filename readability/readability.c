56828f457e3acc96cdfd0858e0d4a4c3fcb15a54
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2022-09-16 03:51:48 +0000

    ddd, Sep 9/16/2022, YYYY, 3:51 A +0

--- readability/readability.c
+++ readability/readability.c
@@ -1,10 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
-
+#include <string.h>
+#include <ctype.h>
+int compute_letters (string text);
 int main(void)
 {
 string text = get_string("Text:");
-
+int letters = compute_letters(text);
+printf("%i\n",letters);
 }
 
 
@@ -18,5 +21,14 @@ string text = get_string("Text:");
 
 int compute_letters (string text)
 {
-    
+    int l = strlen(text);
+    int letters = 0;
+    for(int i=0;i<l;i++)
+    {
+        if(text[i]>='a' && text[i]<='z')
+        letters = letters+1;
+        else if(text[i]>'A' && text[i]<'Z')
+        letters = letters+1;
+    }
+ return letters;
 }
