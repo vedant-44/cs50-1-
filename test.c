ee1e6f0c414664106b686fae8cca1b7e86afc0ce
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2022-09-16 03:56:12 +0000

    ddd, Sep 9/16/2022, YYYY, 3:56 A +0

--- test.c
+++ test.c
@@ -4,16 +4,6 @@
 #include<ctype.h>
 int main (void)
 {
-    string word = get_string("word:");
-    int l = strlen(word);
-    int letters = 0;
-    for(int i=0;i<l;i++)
-    {
-        if(word[i]>='a' && word[i]<='z')
-        letters = letters+1;
-        else if(word[i]>'A' && word[i]<'Z')
-        letters = letters+1;
-    }
-printf("%i\n",letters);
+   
 
 }
