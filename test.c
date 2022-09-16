c90eef7e67cffbcce6cda9e1d548fa61017013ef
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2022-09-16 03:46:55 +0000

    ddd, Sep 9/16/2022, YYYY, 3:46 A +0

--- test.c
+++ test.c
@@ -9,7 +9,7 @@ int main (void)
     int letters = 0;
     for(int i=0;i<l;i++)
     {
-        if(word[i]>'a' && word[i]<'z')
+        if(word[i]>='a' && word[i]<='z')
         letters = letters+1;
         else if(word[i]>'A' && word[i]<'Z')
         letters = letters+1;
