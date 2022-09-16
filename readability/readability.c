772219aeff0eae2090f3b88e206534ae02bd4131
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2022-09-16 04:31:35 +0000

    ddd, Sep 9/16/2022, YYYY, 4:31 A +0

--- readability/readability.c
+++ readability/readability.c
@@ -1,17 +1,18 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
 int count_letters (string text);
 int count_words (string text);
 int count_sentences (string text);
 int main(void)
 {
-string text = get_string("Text:");
-int letters = count_letters(text);
-int words   = count_words(text);
+string text =   get_string("Text:");
+int letters =   count_letters(text);
+int words   =   count_words(text);
+int sentences = count_sentences(text);
 printf("%i\n",letters);
 printf("%i\n",words);
+printf("%i\n",sentences);
 }
 
 
@@ -53,6 +54,13 @@ int count_words (string text)
 
 int count_sentences (string text)
 {
-  
+  int l = strlen(text);
+  int count = 0;
+  for (int i=0; i<l ;i++)
+  {
+    if(text[i]==46 || text[i]==63 || text[i]==33)
+    count = count+1;
+  }
+  return count;
 }
 
