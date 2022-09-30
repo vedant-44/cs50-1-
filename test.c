6dc9a338b8d4e028cee585ea29dc528046b5c520
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2022-09-30 04:04:45 +0000

    ddd, Sep 9/30/2022, YYYY, 4:04 A +0

--- test.c
+++ test.c
@@ -7,6 +7,6 @@ int main (void)
     int letters = get_int("letters:");
     int words = get_int("words:");
     int sentences= get_int("sentences:");
-  float index = 0.0588*(letters/words)-0.296*(sentences/words);
+  float index = 0.0588*(letters*100.0/words)-0.296*(sentences*100.0/words);
   printf("%f\n",index);
 }
