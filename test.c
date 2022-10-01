8527fdeacc65eede565884bf19b8f0a6cf913899
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2022-10-01 06:58:02 +0000

    ddd, Oct 10/1/2022, YYYY, 6:58 A +0

--- test.c
+++ test.c
@@ -1,8 +1,15 @@
 #include<cs50.h>
 #include<stdio.h>
+#include<string.h>
 int main (void)
 {
     alphabets[26] = {a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z};
     string key = get_string("key:");
-    
+    string text = get_string("plaintext:");
+    int l = strlen(text);
+    int x = 0;
+    for(int i=0,i<l,i++)
+    {
+      int x = text[i]-97;
+    }
 }
