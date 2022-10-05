65d00c31fdc14ea5b885db6f8e7a3eb8f75df1ee
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2022-10-05 06:43:33 +0000

    ddd, Oct 10/5/2022, YYYY, 6:43 A +0

--- test.c
+++ test.c
@@ -1,7 +1,7 @@
 #include<cs50.h>
 #include<stdio.h>
 #include<string.h>
-int main (void)
+int main (int argc , string argv)
 {
     char alphabets[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
     string key = get_string("key:");
