38dfe1afa919948781b9f6b2d3440bdd44cc5431
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2022-11-08 13:35:52 +0000

    ddd, Nov 11/8/2022, YYYY, 1:35 A +0

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,7 +1,48 @@
-#include <cs50.h>
-#include <stdio.h>
-
-int main()
+#include<cs50.h>
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+int main(int argc , string argv[])
 {
+    string key;
+    char alphabets[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    if(strlen(argv[1])<26)
+    {
+      printf("Key must contain 26 characters.");
+      printf("\n");
+      return 1;
+    }
 
+    else if (strlen(argv[1])==26)
+    {
+       key = argv[1];
+    }
+    else
+    {
+      printf("Usage: ./substitution key");
+      return 2;
+
+    }
+    string text = get_string("Plaintext:");
+
+    printf("ciphertext:");
+    int l = strlen(text);
+    for(int i=0;i<l;i++)
+    {
+      if(text[i]>='a' && text[i]<='z')
+      {
+      int x = text[i]-'a';
+      printf("%c",tolower(key[x]));
+      }
+      else if(text[i]>='A' && text[i]<='Z')
+      {
+      int x = text[i]-'A';
+      printf("%c",toupper(key[x]));
+      }
+      else
+      {
+        printf("%c",text[i]);
+      }
+    }
+    printf("\n");
 }
