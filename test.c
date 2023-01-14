a087b491a0e55a67874cad6e65d67aab3152c827
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-01-14 08:02:28 +0000

    ddd, Jan 1/14/2023, YYYY, 8:02 A +0

--- test.c
+++ test.c
@@ -1,8 +1,17 @@
 #include<stdio.h>
 #include<cs50.h>
 #include<string.h>
+typedef struct
+{
+    string name;
+    int votes;
+    bool eliminated;
+}
+candidates;
+int preferences[100][9];
 int main (int argc,string argv[])
 {
     
+
 
 }
