53105111f70f2be18d66d993dfde676e84d4f763
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-01-14 08:07:29 +0000

    ddd, Jan 1/14/2023, YYYY, 8:07 A +0

--- test.c
+++ test.c
@@ -8,10 +8,19 @@ typedef struct
     bool eliminated;
 }
 candidates;
+candidate candidates[9];
 int preferences[100][9];
+voter_count=3;
+candidate_count=3;
 int main (int argc,string argv[])
 {
-    
+    for(int i=0;i<3;i++)
+    {
+        candidates[i].name=argv[i+1];
+        candidates[i].votes=0;
+        candidates[i].eliminated=false;
+    }
+
 
 
 }
