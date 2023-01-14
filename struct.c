9d6a37284e2c62d6c5f24f2ab9fb8fa5e60ea2ff
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-01-14 06:22:59 +0000

    ddd, Jan 1/14/2023, YYYY, 6:22 A +0

---
+++ struct.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<cs50.h>
+#include<string.h>
+typedef struct
+{
+    string name;
+    int age;
+}
+person;
+int main(int argc,string argv[])
+{
+string name=argv[1];
+
+
+}
