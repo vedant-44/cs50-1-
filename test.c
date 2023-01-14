0580961dfc45e535ee3ac47460c078698438e261
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-01-14 08:11:42 +0000

    ddd, Jan 1/14/2023, YYYY, 8:11 A +0

--- test.c
+++ test.c
@@ -20,7 +20,13 @@ int main (int argc,string argv[])
         candidates[i].votes=0;
         candidates[i].eliminated=false;
     }
-    
+    for(int j=0;j<3;j++);
+    {
+        for(int k=0;k<3;k++)
+        {
+            printf("Rank%i:",k+1);
+        }
+    }
 
 
 
