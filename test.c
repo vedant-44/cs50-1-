bebbc5c310ec77b8857eb8b7720292946d2bde93
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-01-14 06:40:20 +0000

    ddd, Jan 1/14/2023, YYYY, 6:40 A +0

--- test.c
+++ test.c
@@ -1,37 +1,7 @@
 #include<stdio.h>
-#include<cs50.h>
-#include<string.h>
-bool vote(int rank,string name,int ranks[]);
-string candidates[1000];
-int main(int argc,string argv[])
+int main (void);
 {
-    for(int i=0;i<3;i++)
-    {
-        argv[i+1]=candidates[i];
-    }
-
-for (int i = 0; i <3; i++)
-    {
-        int ranks[1000];
-        for(int j = 0; j <3; j++)
-        {
-            string name = get_string("Rank %i: ", j + 1);
-
-            if (!vote(j, name, ranks))
-            {
-                printf("Invalid vote.\n");
-                return 3;
-            }
-        }
-
-
-         printf("%i,%i,%i",ranks[0],ranks[1],ranks[2]);
-        printf("\n");
-    }
+    int x=2;
+    int y==2;
+    printf("%i,%i",x,y);
 }
-    bool vote(int rank,string name,int ranks[])
-    {
-        return false;
-
-
-    }
