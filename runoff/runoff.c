ebdf2647e533c976c0223758fcb5e7532240dad2
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-01-19 07:32:41 +0000

    Thu, Jan 19, 2023, 7:32 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -154,27 +154,56 @@ for(int i=0;i<candidate_count;i++)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
-    // TODO
-    return false;
+    for(int i=0;i<candidate_count;i++)
+{
+    if(candidates[i].votes>voter_count/2)
+    {
+        printf("%s\n",candidates[i].name);
+        return true;
+    }
+}
+return false;
 }
 
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-    // TODO
-    return 0;
+int min=1000;
+    for(int i=0;i<candidate_count;i++)
+    {
+        if(candidates[i].eliminated==false)
+        {
+            if(candidates[i].votes<=min)
+            min=candidates[i].votes;
+
+        }
+    }
+   return min;
 }
 
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
-    // TODO
-    return false;
+    for(int i=0;i<candidate_count;i++)
+    {
+        if(candidates[i].eliminated==false)
+        {
+            if(candidates[i].votes!=min)
+            return false;
+        }
+    }
+    return true;
 }
 
 // Eliminate the candidate (or candidates) in last place
 void eliminate(int min)
 {
-    // TODO
-    return;
+for(int i=0;i<candidate_count;i++)
+{
+    if(candidates[i].eliminated==false)
+    {
+        if(candidates[i].votes==min)
+        candidates[i].eliminated=true;
+    }
+}
 }
