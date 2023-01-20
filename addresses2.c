b3537e243a98956b432f11e31f96f674de6848ea
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-01-20 09:07:26 +0000

    Fri, Jan 20, 2023, 9:07 AM +00:00

--- addresses2.c
+++ addresses2.c
@@ -3,9 +3,9 @@ int main(void)
 {
     char* s="HI!";
     printf("%s\n",s);
-    printf("%s\n",s[0]);
-    printf("%s\n",*s);
-    printf("%s\n",*(s+1));
-    printf("%s\n",*(s+2));
-    printf("%s\n",*(s+3));
+    printf("%c\n",s[0]);
+    printf("%c\n",*s);
+    printf("%c\n",*(s+1));
+    printf("%c\n",*(s+2));
+    printf("%c\n",*(s+3));
 }
