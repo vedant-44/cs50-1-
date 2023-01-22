a855fc07883bdd4cb4730dde802b85714d2fff3b
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-01-22 04:56:45 +0000

    Sun, Jan 22, 2023, 4:56 AM +00:00

--- lecture4/swap.c
+++ lecture4/swap.c
@@ -1,20 +1,19 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<cs50.h>
- void swap(int x, int y);
+ void swap();
  int x=1;
  int y=2;
 int main(void)
 {
-    int x=1;
-    int y=2;
+
     printf("%i,%i\n",x,y);
     swap(x,y);
      printf("%i,%i\n",x,y);
 
 
 }
- void swap(int x, int y)
+ void swap()
  {
    int temp= x;
    x=y;
