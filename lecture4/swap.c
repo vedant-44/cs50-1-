aaefca5ced3769718e454d063a916305dcf72e16
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-01-22 04:54:55 +0000

    Sun, Jan 22, 2023, 4:54 AM +00:00

--- lecture4/swap.c
+++ lecture4/swap.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<cs50.h>
- void swap(int a, int b);
+ void swap(int x, int y);
+ int x=1;
+ int y=2;
 int main(void)
 {
     int x=1;
@@ -12,9 +14,9 @@ int main(void)
 
 
 }
- void swap(int a, int b)
+ void swap(int x, int y)
  {
-   int temp= a;
-   a=b;
-   b=temp;
+   int temp= x;
+   x=y;
+   y=temp;
  }
