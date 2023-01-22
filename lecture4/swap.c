2f63c43a0fae9cd62a61cbb3ac53d3f06117a0ee
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-01-22 05:12:12 +0000

    Sun, Jan 22, 2023, 5:12 AM +00:00

--- lecture4/swap.c
+++ lecture4/swap.c
@@ -1,19 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<cs50.h>
- void swap();
+void swap(int*a ,int*b);
 int main(void)
 {
-
+    int x=1;
+    int y=2;
+    int* k=&x;
+    int* l=&y;
     printf("%i,%i\n",x,y);
-    swap(x,y);
+    swap(k,l);
      printf("%i,%i\n",x,y);
 
 
 }
- void swap()
- {
-   int temp= x;
-   x=y;
-   y=temp;
+ void swap(int* a,int* b )
+{
+  
+
  }
