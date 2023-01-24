e7c87ff7011ba30c2f17e81546c16c12e2f6b1e9
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-01-24 03:11:53 +0000

    Tue, Jan 24, 2023, 3:11 AM +00:00

--- a.c
+++ a.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
 int main(void)
 {
-    
+    typedef struct
+    {
+        int a;
+        char b;
+    }
+    st;
+    st person[2];
+    person[0].a=5;
+    printf("%d",person[0].a);
 }
