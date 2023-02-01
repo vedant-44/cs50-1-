57d9c8ebd5e890745f7675d4b4482b376ffe793b
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-02-01 05:50:47 +0000

    Wed, Feb 1, 2023, 5:50 AM +00:00

--- test.c
+++ test.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
 int main(void)
 {
+  typedef struct
+  {
+    char arr[10];
+    int age[10];
+  }
+person;
+person people;
+people.arr[0]='a';
 
-    char arr[]="abcd";
-    for(int i=0;i<4;i++)
-    {
-        printf("Alphabet\n");
-        scanf("%c",&arr[i]);
-    }
-    printf("%c\n",arr[0]);
+
 
 }
