5de7283795b50c626c6bd3c7ba3a7862afe461d3
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-02-05 02:39:19 +0000

    Sun, Feb 5, 2023, 8:09 AM +05:30

--- lecture4/malloc.c
+++ lecture4/malloc.c
@@ -1,20 +1,16 @@
+
 #include<stdio.h>
-#include<cs50.h>
 #include<string.h>
 #include<ctype.h>
 #include<stdlib.h>
 int main(void)
 {
+  char s[10],a[10];
+ printf("s:\n");
+ scanf("%s",s);
+ printf("%s\n",s);
+ a=s;
 
-    char* a= get_string("a:");
-    char* b= malloc(strlen(a)+1);
-    int n= strlen(a);
-    for(int i=0;i<=n;i++)
-    {
-        b[i]=a[i];
-    }
-    b[0] = toupper(b[0]);
-    printf("a:%s\nb:%s\n",a,b);
-    free(b);
 
 }
+
