27a10e1af9dec050edf72701380611e760b19c3e
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-02-05 02:52:30 +0000

    Sun, Feb 5, 2023, 8:22 AM +05:30

--- lecture4/malloc.c
+++ lecture4/malloc.c
@@ -5,7 +5,14 @@
 #include<stdlib.h>
 int main(void)
 {
-  
+  char* s,a = NULL;
+  printf("s:\n");
+  scanf("%s",s);
+  a = malloc(strlen(s)+1);
+  *a=*s;
+  a[0]=toupper(a[0]);
+  printf("s:%s\na:%s\n",s,a);
+
 
 
 
