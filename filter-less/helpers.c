fce95ef80265014143b3011534556f4b1ade1ca3
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-02-05 06:40:33 +0000

    Sun, Feb 5, 2023, 12:10 PM +05:30

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -3,7 +3,17 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-  
+  for(int i=0;i<height;i++)
+  {
+    for(int j=0;j<width;j++)
+    {
+        int average = (image[i][j].rgbtRed + image[i][j].rgbtBlue image[i][j].rgbtGreen)/3;
+        image[i][j].rgbtRed=average;
+        image[i][j].rgbtBlue=average;
+        image[i][j].rgbtGreen=average;
+    }
+  }
+
 }
 
 // Convert image to sepia
