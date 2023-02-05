2dc802ba0a1d35f7b248853d3091a4b134a59551
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-02-05 04:08:01 +0000

    Sun, Feb 5, 2023, 9:38 AM +05:30

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -6,7 +6,10 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
     {
         for(int j=0;j<width;j++)
         {
-            if(image[i][j].rgbtBlue==0 && image[i][j].rgbtBlue==0  && image[i][j].rgbtBlue==0 )
+            if(image[i][j].rgbtBlue==0 && image[i][j].rgbtGreen==0  && image[i][j].rgbtRed==0 )
+            {
+                image[i][j].rgbtRed=255;
+            }
         }
     }
 }
