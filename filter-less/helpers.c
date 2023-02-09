f8f041cbf3cb573b88f27fcc4d13b17860c86493
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-02-09 16:31:16 +0000

    Thu, Feb 9, 2023, 10:01 PM +05:30

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -75,10 +75,16 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    for(inti=0;i<height;i++)
+    {
+        for(intj=0;j<width;j++)
+        {
    if(i!=0 && i!=height-1 && j!=0 && j!=width-1)
    {
 image[i][j].rgbtRed=(image[i][j].rgbtRed+image[i][j+1].rgbtRed+image[i][j-1].rgbtRed+image[i+1][j-1].rgbtRed+image[i-1][j-1].rgbtRed+image[i][j-1].rgbtRed+image[i-1][j+1].rgbtRed+image[i][j+1].rgbtRed+image[i+1][j+1].rgbtRed)/9;
 image[i][j].rgbtBlue=(image[i][j].rgbtBlue+image[i][j+1].rgbtBlue+image[i][j-1].rgbtBlue+image[i+1][j-1].rgbtBlue+image[i-1][j-1].rgbtBlue+image[i][j-1].rgbtBlue+image[i-1][j+1].rgbtBlue+image[i][j+1].rgbtBlue+image[i+1][j+1].rgbtBlue)/9;
 image[i][j].rgbtGreen=(image[i][j].rgbtGreen+image[i][j+1].rgbtGreen+image[i][j-1].rgbtGreen+image[i+1][j-1].rgbtGreen+image[i-1][j-1].rgbtGreen+image[i][j-1].rgbtGreen+image[i-1][j+1].rgbtGreen+image[i][j+1].rgbtGreen+image[i+1][j+1].rgbtGreen)/9;
    }
+        }
+    }
 }
