c7dd61bd89895dd204ac9c2b24be2c32d3111743
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-02-11 06:59:10 +0000

    Sat, Feb 11, 2023, 12:29 PM +05:30

--- recover/recover.c
+++ recover/recover.c
@@ -4,11 +4,13 @@ typedef uint8_t byte;
 
 int main(int argc, char *argv[])
 {
+    
     if(argc<2 || argc>2)
     {
         return 1;
     }
     FILE *mc=fopen(argv[1],"r");
+    while(fread())
 
 
 }
