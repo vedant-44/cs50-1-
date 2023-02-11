1ab1200aa511333fe3c355a7900860c8dbd0ba0c
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-02-11 14:40:22 +0000

    Sat, Feb 11, 2023, 8:10 PM +05:30

--- test.c
+++ test.c
@@ -18,3 +18,31 @@ int main(void)
     fprintf(img,"\nabhinav");
 
 }
+ if((arr[0]!=0xff || arr[1]!=0xd8 || arr[2]!=0xff || (arr[3]&0xf0)!=0xe0) && flag==false)
+        {
+            continue;
+        }
+
+
+
+       else if(arr[0]==0xff || arr[1]==0xd8 || arr[2]==0xff || (arr[3]&0xf0)==0xe0)
+        {
+          sprintf(photo,"%03i.jpg",i);
+          FILE* img=fopen(photo,"w");
+          fwrite(arr,1,512,img);
+          i++;
+          flag =true;
+        }
+        else
+        {
+            FILE* img=fopen(photo,"a");
+            fwrite(arr,1,512,img);
+        }
+
+
+    }
+free(photo);
+
+
+
+}
