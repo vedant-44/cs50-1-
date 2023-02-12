2dfe322f1841fe7e40b5da88391d1f13579d4921
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-02-12 05:54:38 +0000

    Sun, Feb 12, 2023, 11:24 AM +05:30

---
+++ test1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+    char* photo=malloc(100);
+    FILE* img=NULL;
+    for(int i=0;i<2;i++)
+    {
+        if(i==0)
+        {
+            sprintf(photo,"%03i.csv",0);
+            img=fopen
+        }
+    }
+
+}
