74259843dbef283862036d68e93f535f5db25dce
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-02-12 06:33:06 +0000

    Sun, Feb 12, 2023, 12:03 PM +05:30

--- recover/recover.c
+++ recover/recover.c
@@ -1,60 +1,66 @@
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+typedef uint8_t byte;
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
+    unsigned char *arr=malloc[512];
+    bool flag=false;
+    char photo[1000];
+    int i=1;
+    FILE* img=NULL;
+
+
+
+    if(argc<2 || argc>2)
     {
-        fprintf(stderr, "Usage: ./recover image\n");
         return 1;
     }
 
-    FILE *file = fopen(argv[1], "r");
-    if (file == NULL)
-    {
-        fprintf(stderr, "Could not open file %s.\n", argv[1]);
-        return 1;
-    }
+    FILE *mc=fopen(argv[1],"r");
 
-    FILE *img;
-    char filename[1000];
-    unsigned char *bf = malloc(512);
-    int counter = 0;
-
-    while (fread(bf, 512, 1, file))
+    while(fread(arr,1,512,mc)==512)
     {
-        if (bf[0] == 0xff && bf[1] == 0xd8 && bf[2] == 0xff && (bf[3] & 0xf0) == 0xe0)
+        if((arr[0]!=0xff || arr[1]!=0xd8 || arr[2]!=0xff || (arr[3]&0xf0)!=0xe0) && flag==false)
         {
-            if (counter > 0)
+            continue;
+        }
+        if(arr[0]==0xff || arr[1]==0xd8 || arr[2]==0xff || (arr[3]&0xf0)==0xe0)
+        {
+            if(flag==false)
+            {
+                sprintf(photo,"%03i.jpg",0);
+                img=fopen(photo,"w");
+                fwrite(arr,1,512,img);
+                flag=true;
+            }
+            else
             {
                 fclose(img);
+                sprintf(photo,"%03i.jpg",i);
+                img=fopen(photo,"w");
+                fwrite(arr,1,512,img);
+                i++;
             }
-
-            // create filename
-            sprintf(filename, "%03d.jpg", counter);
-            // open new image file
-            img = fopen(filename, "w");
-
-            if (img == NULL)
-            {
-                fclose(file);
-                free(bf);
-                fprintf(stderr, "Could not create output JPG %s", filename);
-                return 3;
-            }
-
-            counter++;
         }
-        if (counter > 0)
+        else
         {
-            fwrite(bf, 512, 1, img);
+            img=fopen(photo,"a");
+            fwrite(arr,1,512,img);
         }
     }
 
-    fclose(img);
-    fclose(file);
-    free(bf);
-    return 0;
+free(arr);
+
+
+
+
+
+
+
 }
 
 
