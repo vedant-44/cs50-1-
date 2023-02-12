9feab37d1ae3b0ea4cd8079060ffbf5d4ce6b528
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-02-12 05:17:31 +0000

    Sun, Feb 12, 2023, 10:47 AM +05:30

--- recover/recover.c
+++ recover/recover.c
@@ -10,6 +10,7 @@ int main(int argc, char *argv[])
     bool flag=false;
     char* photo=malloc(100);
     int i=0;
+    FILE* img=NULL;
 
 
 
@@ -22,9 +23,29 @@ int main(int argc, char *argv[])
 
     while(fread(arr,1,512,mc)==512)
     {
-        
-    }
+        if((arr[0]!=0xff || arr[1]!=0xd8 || arr[2]!=0xff || (arr[3]&0xf0)!=0xe0) && flag==false)
+        {
+            continue;
+        }
 
+
+
+       else if(arr[0]==0xff || arr[1]==0xd8 || arr[2]==0xff || (arr[3]&0xf0)==0xe0)
+        {
+          sprintf(photo,"%03i.jpg",i);
+           img=fopen(photo,"w");
+          fwrite(arr,1,512,img);
+          i++;
+          flag =true;
+        }
+        else
+        {
+            FILE* img=fopen(photo,"a");
+            fwrite(arr,1,512,img);
+        }
+
+    }
+}
 
 
 
