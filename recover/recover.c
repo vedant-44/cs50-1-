6416c941778a15f63a7110b518d2e05446beae91
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-02-13 02:44:58 +0000

    Mon, Feb 13, 2023, 8:14 AM +05:30

--- recover/recover.c
+++ recover/recover.c
@@ -1,53 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
 typedef uint8_t byte;
 
 int main(int argc, char *argv[])
 {
     byte arr[512];
+    bool flag=false;
     char* photo=malloc(8);
+    int i=1;
     FILE* img=NULL;
-    int counter=0;
 
 
 
-    if(argc!=1)
+    if(argc<2 || argc>2)
     {
         return 1;
-    }
-    if(argv[1]==NULL)
-    {
-        return 2;
     }
 
     FILE *mc=fopen(argv[1],"r");
 
-    while(fread(arr,512,1,mc)==1)
+    while(fread(arr,1,512,mc)==512)
     {
-
-
-
-
-
-        if(arr[0]==0xff || arr[1]==0xd8 || arr[2]==0xff || (arr[3]&0xf0)==0xe0)
+        if((arr[0]!=0xff || arr[1]!=0xd8 || arr[2]!=0xff || (arr[3]&0xf0)!=0xe0) && flag==false)
         {
-            if(counter !=0)
+            continue;
+        }
+        if(arr[0]==0xff && arr[1]==0xd8 && arr[2]==0xff && (arr[3]&0xf0)==0xe0)
+        {
+            if(flag==false)
+            {
+                sprintf(photo,"%03i.jpg",0);
+                img=fopen(photo,"w");
+                fwrite(arr,1,512,img);
+                flag=true;
+            }
+            else
             {
                 fclose(img);
+                sprintf(photo,"%03i.jpg",i);
+                img=fopen(photo,"w");
+                fwrite(arr,1,512,img);
+                i++;
             }
-            sprintf(photo,"%03i.jpg",counter);
-            img=fopen(photo,"w");
-            fwrite(arr,1,512,img);
-            counter++;
         }
-        else if(counter>0)
+        else
         {
+
             fwrite(arr,1,512,img);
         }
     }
-    free(photo);
+
+
+
+
+
+
+
+free(photo);
+
 }
+
 
 
 
