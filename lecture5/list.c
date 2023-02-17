48bb3a09672a9ce77f49f9aec3fcd8cce15f606f
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-02-17 13:23:51 +0000

    Fri, Feb 17, 2023, 6:53 PM +05:30

--- lecture5/list.c
+++ lecture5/list.c
@@ -2,5 +2,16 @@
 #include<stdlib.h>
 int main(int argc,char*argv[])
 {
-    
+    typedef struct node
+    {
+        int number;
+        node* next;
+    }
+    node;
+    for(int i=0;i<argc-1;i++)
+    {
+        node* list=NULL;
+        node* n=malloc(sizeof(node));
+        n->number=
+    }
 }
