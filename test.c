3402bf469a863fc1382a673afc1b5a7c033ae5a1
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-02-18 05:03:57 +0000

    Sat, Feb 18, 2023, 10:33 AM +05:30

---
+++ test.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+    int* ptr =malloc(2*sizeof(int));
+    *ptr=345;
+    printf("%i\n",ptr);
+    free(ptr);
+    printf("%i\n",ptr);
+
+
+}
