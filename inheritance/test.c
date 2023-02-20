f10fe7e5793f5fa5d05f31f6a032d0017ae4f3ab
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-02-20 00:09:37 +0000

    Mon, Feb 20, 2023, 5:39 AM +05:30

---
+++ inheritance/test.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+const int generations=3;
+typedef struct person
+{
+    struct person* parents[2];
+    char alleles[2];
+}
+person* createfamily(generations);
+int main(void)
+{
+
+
+}
+p* createfamily(generations)
+{
+
+}
