3a8f6b999e01076054cc322dd523e2bcc6dd8bf4
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-02-20 00:59:55 +0000

    Mon, Feb 20, 2023, 6:29 AM +05:30

--- inheritance/test.c
+++ inheritance/test.c
@@ -1,15 +1,22 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<stdbool.h>
+#include<time.h>
 const int generations=3;
+const int INDENT_LENGTH = 4;
 typedef struct person
 {
     struct person* parents[2];
     char alleles[2];
 }
 person* createfamily(generations);
+char random_allele();
 int main(void)
 {
+    srand(time(0));
+
+
+
 
 
 }
@@ -26,6 +33,7 @@ else
 {
     p->parents[0]=NULL;
     p->parents[1]=NULL;
+
 }
 return p;
 }
