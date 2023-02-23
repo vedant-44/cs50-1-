9320aac4c49d91280079e826a6429d1181700d82
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-02-23 00:29:17 +0000

    Thu, Feb 23, 2023, 5:59 AM +05:30

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -16,7 +16,7 @@ typedef struct node
 node;
 
 // TODO: Choose number of buckets in hash table
-const unsigned int N = 26;
+const unsigned int N = 256;
 
 // Hash table
 node *table[N];
@@ -32,7 +32,7 @@ bool check(const char *word)
 unsigned int hash(const char *word)
 {
     // TODO: Improve this hash function
-    return toupper(word[0]) - 'A';
+
 }
 
 // Loads dictionary into memory, returning true if successful, else false
