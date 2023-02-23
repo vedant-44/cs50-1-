bfc9f16da9af1524716008ffc6c7d805dec01599
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-02-23 00:50:46 +0000

    Thu, Feb 23, 2023, 6:20 AM +05:30

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -24,7 +24,7 @@ node *table[N];
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
 {
-   for(int i=0,k=strlen(word);,;i<l;i++)
+   for(int i=0;i<l;i++)
    {
 
    }
