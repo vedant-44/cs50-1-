093130bc4b2cc360e5d0a7047a9b4af58a9e4927
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-02-24 01:17:14 +0000

    Fri, Feb 24, 2023, 6:47 AM +05:30

---
+++ speller/test.c
@@ -0,0 +1,28 @@
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dictionary.h"
+
+
+typedef struct node
+{
+    char word[LENGTH + 1];
+    struct node *next;
+}
+node;
+
+// TODO: Choose number of buckets in hash table
+const unsigned int N = 26;
+
+// Hash table
+node *table[N];
+int main(void)
+{
+
+}
+bool load(const char* dictionary)
+{
+
+}
