70e8bf0551b724c04d052aa363b309ee4098cf87
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-02-24 02:02:17 +0000

    Fri, Feb 24, 2023, 7:32 AM +05:30

---
+++ speller/test3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+   FILE *fp;
+
+   char buff[255];
+   fp = fopen("dictionaries/less.txt", "r");
+   while(fscanf(fp, "%s", buff)!=EOF){
+   printf("%s ", buff );
+   }
+   fclose(fp);
+}
