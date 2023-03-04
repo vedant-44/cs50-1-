b65ecc5c984d88bae8af4ead67c9fd50db5239b4
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-03-04 05:20:50 +0000

    Sat, Mar 4, 2023, 10:50 AM +05:30

--- lecture6a/phonebook.py
+++ lecture6a/phonebook.py
@@ -2,9 +2,7 @@ from sys import exit
 name=input("Name:")
 name=name.capitalize()
 dictionary={'Vedant':'453432','Abhinav':'325234'}
-for i in dictionary:
-    if(i==name):
-        print(f"Number: {dictionary[name]}")
-        exit(0)
-exit(1)
+if name in dictionary:
+    print(f"Number:{dictionary[name]}")
+
 
