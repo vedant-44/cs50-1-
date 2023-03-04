aa259c064180c89794b63c5cb9e32b44744214f6
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-03-04 05:49:23 +0000

    Sat, Mar 4, 2023, 11:19 AM +05:30

--- lecture6a/data.py
+++ lecture6a/data.py
@@ -1,7 +1,7 @@
 import csv
 file=open("person.csv","a")
-name=input("Name")
-age=input("Age")
+name=input("Name: ")
+age=input("Age: ")
 writer=csv.writer(file)
 writer.writerow([name,age])
 file.close()
