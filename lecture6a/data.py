521548b8d3f623853dbfcc55f52650610f6777dd
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-03-04 05:58:21 +0000

    Sat, Mar 4, 2023, 11:28 AM +05:30

--- lecture6a/data.py
+++ lecture6a/data.py
@@ -1,3 +1,7 @@
 import csv
 file=open("person.csv","a")
-name=
+name=input("Name: ")
+age=input("Age: ")
+
+write=csv.DictWriter(file,fieldnames=["name","age"])
+write.writerow({"name":name,"age":age})
