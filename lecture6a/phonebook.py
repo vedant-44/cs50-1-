45eb2d5f65796049473c48bb7bb432de73602342
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-03-04 04:44:58 +0000

    Sat, Mar 4, 2023, 10:14 AM +05:30

--- lecture6a/phonebook.py
+++ lecture6a/phonebook.py
@@ -3,5 +3,7 @@ if len(sys.argv)<2:
     print("No command line argument")
     sys.exit(1)
 else:
-    print(f"{sys.argv[1]}")
+    for i in sys.argv[1:]:
+       print(f"{i}",end='')
+    print()
     sys.exit(0)
