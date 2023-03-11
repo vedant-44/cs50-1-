90168170dece1883e92e734446191be7dfa28566
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-03-11 04:06:51 +0000

    Sat, Mar 11, 2023, 9:36 AM +05:30

--- world-cup/test.py
+++ world-cup/test.py
@@ -1,7 +1,8 @@
 import csv
-lsteams[]
+lsteams=[]
 with open('2018m.csv') as csv_file:
-    reader=csv.Dictreader(csv_file,fieldnames=['team','rating'])
+    reader=csv.DictReader(csv_file,fieldnames=['team','rating'])
     for row in reader:
         team={'team':'rating'}
         lsteams.append(team)
+print(lsteams[1])
