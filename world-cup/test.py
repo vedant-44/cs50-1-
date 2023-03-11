4ede56b4280e3a862862c2120d7331ed53832817
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-03-11 05:53:57 +0000

    Sat, Mar 11, 2023, 11:23 AM +05:30

--- world-cup/test.py
+++ world-cup/test.py
@@ -1,9 +1,29 @@
 import csv
-lsteams=[]
-with open('2018m.csv') as csv_file:
-    reader=csv.DictReader(csv_file)
-    for row in reader:
+teams=[]
+def main():
+
+ with open('2018m.csv') as csv_file:
+     reader=csv.DictReader(csv_file)
+     for row in reader:
         team={row['team']:row['rating']}
-        lsteams.append(team)
+        teams.append(team)
+
+
+
+
+
+def simulate_round(teams):
+    """Simulate a round. Return a list of winning teams."""
+    winners = []
+
+    # Simulate games for all pairs of teams
+    for i in range(0, len(teams), 2):
+        if simulate_game(teams[i], teams[i + 1]):
+            winners.append(teams[i])
+        else:
+            winners.append(teams[i + 1])
+
+    return winners
+
 
 
