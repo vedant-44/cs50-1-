3bf38be0a5d1cfee01bcc807936f7e8b7c5b0d2c
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-03-11 04:44:35 +0000

    Sat, Mar 11, 2023, 10:14 AM +05:30

--- world-cup/test.py
+++ world-cup/test.py
@@ -1,13 +1,9 @@
 import csv
 lsteams=[]
 with open('2018m.csv') as csv_file:
-    reader=csv.reader(csv_file,delimiter=',')
-    line_count=0
+    reader=csv.DictReader(csv_file)
     for row in reader:
-          if line_count==0:
-             line_count+=1
-          else:
-              team={row[0]:row[1]}
-              lsteams.append(team)
+        team={row['team']:row['rating']}
+        lsteams.append(team)
 print(lsteams[0])
 
