0079bd48147bd04f98e4a797867e300b312eed02
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-03-11 04:25:15 +0000

    Sat, Mar 11, 2023, 9:55 AM +05:30

--- world-cup/test.py
+++ world-cup/test.py
@@ -1,6 +1,6 @@
 import csv
 lsteams=[]
 with open('2018m.csv') as csv_file:
-    reader=csv.DictReader(csv_file)
+    reader=csv.reader(csv_file,delimiter=',')
     for row in reader:
         print(row[0])
