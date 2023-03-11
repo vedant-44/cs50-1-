d1375e487001ca2391f5bae3719740651f10a6e9
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-03-11 04:29:31 +0000

    Sat, Mar 11, 2023, 9:59 AM +05:30

--- world-cup/test.py
+++ world-cup/test.py
@@ -2,5 +2,12 @@ import csv
 lsteams=[]
 with open('2018m.csv') as csv_file:
     reader=csv.reader(csv_file,delimiter=',')
+    line_count=0
     for row in reader:
-        print(row[0])
+          if line_count==0:
+             line_count+=1
+          else:
+              team={row[0]:row[1]}
+              lsteams.append(team)
+print(lsteams[0])
+
