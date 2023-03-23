1ed60bd4f6161ed1cb7916787cea8a53f3afee92
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-03-23 04:20:59 +0000

    Thu, Mar 23, 2023, 9:50 AM +05:30

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,3 +1,10 @@
 from cs50 import get_string
 num=get_string('Number: ')
-l=len(num)
+l=len(num)-1
+sum=0
+while l>=0:
+    sum+=int(num[l])
+    if l-1>=0:
+        sum+=2*int(num[l-1])
+    l=l-2
+
