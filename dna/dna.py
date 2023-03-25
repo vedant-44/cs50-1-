36775c3d6f048beb32560c29e03bb6e51b27eba8
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-03-25 04:16:12 +0000

    Sat, Mar 25, 2023, 9:46 AM +05:30

--- dna/dna.py
+++ dna/dna.py
@@ -5,9 +5,10 @@ import sys
 def main():
 
     # TODO: Check for command-line usage
-       
+       if len(sys.argv) !=3:
+            print('Usage: python dna.py data.csv sequence.txt')
     # TODO: Read database file into a variable
-
+       
     # TODO: Read DNA sequence file into a variable
 
     # TODO: Find longest match of each STR in DNA sequence
