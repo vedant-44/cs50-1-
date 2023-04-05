b215b9930702b9d260f7dd94e3d6614e67a4cd70
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-04-05 02:59:21 +0000

    Wed, Apr 5, 2023, 8:29 AM +05:30

--- lecture7/dictreader.py
+++ lecture7/dictreader.py
@@ -8,7 +8,7 @@ with open('favourites.csv') as file:
             counts[favourite]+=1
         else:
             counts[favourite]=1
-for favourite in sorted(counts,key=lambda a: counts[a],reverse=True):
+for favourite in sorted(counts,key=lambda language: counts[language],reverse=True):
     print(f"{favourite}: {counts[favourite]}")
 
 
