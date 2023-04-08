ca7aa96d5be9bb22a0013fb894e792c88def5f0e
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-04-08 01:55:40 +0000

    Sat, Apr 8, 2023, 7:25 AM +05:30

--- lecture7/sql.py
+++ lecture7/sql.py
@@ -1,5 +1,4 @@
 from cs50 import SQL
 db =SQL('sqlite:///favourites.db')
 language=input('Language: ')
-rows=db.execute("SELECT * FROM favourites")
-print(rows[2]['language'])
+rows=db.execute("SELECT COUNT(*) FROM favourites WHERE language=?",language)
