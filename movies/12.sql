f4962967de16386ee67b07e4a914ae8335beb9ec
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-04-09 02:29:23 +0000

    Sun, Apr 9, 2023, 7:59 AM +05:30

---
+++ movies/12.sql
@@ -0,0 +1,1 @@
+SELECT title FROM movies WHERE id IN(SELECT s1.movie_id FROM stars s1 JOIN stars s2 ON s1.movie_id=s2.movie_id WHERE s1.person_id=(SELECT id FROM people WHERE name='Johnny Depp') AND s2.person_id=(SELECT id FROM people WHERE name='Helena Bonham Carter'));
