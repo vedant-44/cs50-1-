4ddb747c944a28bf32be20f40d2f009d46f5bc11
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-04-10 12:22:27 +0000

    Mon, Apr 10, 2023, 5:52 PM +05:30

---
+++ movies/13.sql
@@ -0,0 +1,1 @@
+SELECT name FROM people WHERE id IN(SELECT s2.person_id FROM stars s1 JOIN stars s2 ON s1.movie_id=s2.movie_id  WHERE s1.person_id=(SELECT id FROM people WHERE name='Kevin Bacon' AND birth=1958)AND s2.person_id IN(SELECT id FROM people WHERE name!='Kevin Bacon'));
