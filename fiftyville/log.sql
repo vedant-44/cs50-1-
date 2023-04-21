3b8270d323ad5f606bb6b8f46ef284e36b6088f9
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-04-21 03:51:42 +0000

    Fri, Apr 21, 2023, 9:21 AM +05:30

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -9,3 +9,5 @@ SELECT * FROM bakery_security_logs WHERE month=7 AND day=28 AND hour=10 AND minu
  SELECT * FROM atm_transactions WHERE month=7 AND day=28 AND atm_location='Leggett Street' AND transaction_type='withdraw';
 --Getting details related to bank accounts.
  SELECT * FROM bank_accounts WHERE account_number IN(SELECT account_number FROM atm_transactions WHERE month=7 AND day=28 AND atm_location='Leggett Street' AND transaction_type='withdraw');
+ --Possible suspect list using current information.
+  SELECT * FROM people WHERE id IN(SELECT person_id FROM bank_accounts WHERE account_number IN(SELECT account_number FROM atm_transactions WHERE month=7 AND day=28 AND atm_location='Leggett Street' AND transaction_type='withdraw')) AND license_plate IN(SELECT license_plate FROM bakery_security_logs WHERE month=7 AND day=28 AND hour=10 AND minute<25 AND minute>15);
