89302403873fb179033ba70d94333cc0ec25c979
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-04-21 03:18:30 +0000

    Fri, Apr 21, 2023, 8:48 AM +05:30

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -3,3 +3,7 @@
 SELECT * FROM crime_scene_reports WHERE month=7 AND day=28;
 --Checking witness interviews.
 SELECT * FROM interviews WHERE day=28 AND month=7;
+--Getting license plate number.
+SELECT * FROM bakery_security_logs WHERE month=7 AND day=28 AND hour=10 AND minute<25 AND minute>15;
+--Checking atm transactions.
+ SELECT * FROM atm_transactions WHERE month=7 AND day=28 AND atm_location='Leggett Street' AND transaction_type='withdraw';
