4c9c161ed67068e098eecdffa643792e38ef9781
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-04-21 03:03:25 +0000

    Fri, Apr 21, 2023, 8:33 AM +05:30

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,3 +1,5 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
---Getting basic  information about the crime
+--Getting basic  information about the crime.
 SELECT * FROM crime_scene_reports WHERE month=7 AND day=28;
+--Checking witness interviews.
+SELECT * FROM interviews WHERE day=28 AND month=7;
