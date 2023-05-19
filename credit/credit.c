b7072694baff79fbbac90e9c51fb7a5441ce6b2f
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-05-19 15:28:52 +0000

    Fri, May 19, 2023, 8:58 PM +05:30

--- credit/credit.c
+++ credit/credit.c
@@ -6,15 +6,15 @@ int main(void)
  int a=123;
  int arr[200];
  int j=0;
- while(a>1)
+ while(a>0)
  {
     arr[j]=a%10;
     a=a/10;
     j++;
 
  }
- for(int i=0;i<3;i++)
+ for(int i=j-1;i>=0;i--)
  {
-    printf("%i\n",arr[j]);
+    printf("%d\n",arr[j]);
  }
 }
