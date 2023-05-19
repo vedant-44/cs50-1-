ed1d5ac39ab0df5fd03fc2cd8bc5ea9125d7c69a
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-05-19 15:31:16 +0000

    Fri, May 19, 2023, 9:01 PM +05:30

--- credit/credit.c
+++ credit/credit.c
@@ -1,20 +1,19 @@
-#include <cs50.h>
 #include <stdio.h>
 
-int main(void)
-{
- int a=123;
- int arr[200];
- int j=0;
- while(a>0)
- {
-    arr[j]=a%10;
-    a=a/10;
-    j++;
+int main(void) {
+    int a = 123;
+    int arr[200];
+    int j = 0;
 
- }
- for(int i=j-1;i>=0;i--)
- {
-    printf("%d\n",arr[j]);
- }
+    while (a > 0) {
+        arr[j] = a % 10;
+        a = a / 10;
+        j++;
+    }
+
+    for (int i = j - 1; i >= 0; i--) {
+        printf("%d\n", arr[i]);
+    }
+
+    
 }
