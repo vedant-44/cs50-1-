33b520947674c457c4cad97b69c2e05cc24c9f53
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-05-19 15:37:38 +0000

    Fri, May 19, 2023, 9:07 PM +05:30

--- credit/credit.c
+++ credit/credit.c
@@ -4,14 +4,18 @@ int main(void) {
     a=get_long("Number:");
     int arr[200];
     int j = 0;
+    int sum=0;
 
     while (a > 0) {
         arr[j] = a % 10;
         a = a / 10;
         j++;
     }
-
+  for(int i=j-1;i>=0;i--)
+  {
     
+  }
+
 
 
 }
