6ca384a799353267ff669df21a9d1f85e50ede8f
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-05-21 11:12:44 +0000

    Sun, May 21, 2023, 4:42 PM +05:30

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded',function(){
     let playbutton=document.getElementById('play');
+    let mutebutton=document.getElementById('mute');
     let video=document.getElementById('video');
     playbutton.addEventListener('click',function(){
         if(video.paused)
@@ -11,6 +12,18 @@ document.addEventListener('DOMContentLoaded',function(){
         {
             video.pause();
             playbutton.textContent='Play';
+        }
+    })
+    mutebutton.addEventListener('click',function(){
+        if(video.muted)
+        {
+            video.muted=false;
+            mutebutton.textContent='Mute';
+        }
+        else
+        {
+            video.muted=true;
+            mutebutton.textContent='Unmute';
         }
     })
 })
