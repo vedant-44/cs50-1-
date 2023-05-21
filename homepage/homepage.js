39c65ed29bb7446fe585589fb094b531c7e8f475
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-05-21 07:01:54 +0000

    Sun, May 21, 2023, 12:31 PM +05:30

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -1,3 +1,6 @@
 document.addEventListener('DOMContentLoaded',function(){
-    
+    let palybutton=document.getElementById('play');
+    playbutton.addEventListener('click',function(){
+        if()
+    })
 })
