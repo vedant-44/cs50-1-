8a315b6bdbcd92aaf754d23718eaf9ddec3f72cb
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-05-27 06:02:20 +0000

    Sat, May 27, 2023, 11:32 AM +05:30

--- hello/app.py
+++ hello/app.py
@@ -4,9 +4,5 @@ app=Flask(__name__)
 
 @app.route("/")
 def index():
-    if 'name' in request.args:
-        name=request.args['name']
-    else:
-        name='world'
-    return render_template('index.html',name=name)
+    return render_template('index.html')
 
