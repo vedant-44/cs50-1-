b192c69b18ab84fe70e8a4cce2507c64dfbd07b0
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-05-27 05:51:34 +0000

    Sat, May 27, 2023, 11:21 AM +05:30

--- hello/app.py
+++ hello/app.py
@@ -1,3 +1,12 @@
 from flask import Flask,render_template,request
 
 app=Flask(__name__)
+
+@app.route("/")
+def index():
+    if name in request.args:
+        name=request.args['name']
+    else:
+        name='world'
+    return 
+
