c8e0b06ed927c40ccf8ed6377f3a62fd11c2a76b
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-05-28 02:31:09 +0000

    Sun, May 28, 2023, 8:01 AM +05:30

--- froshims/app.py
+++ froshims/app.py
@@ -7,9 +7,11 @@ def index():
     return render_template('index.html')
 @app.route("/msg",methods=['POST'])
 def msg():
-     PARTICIPANTS={}
-     
+
+
     return render_template('msg.html')
 @app.route("/participants")
 def participants():
+      PARTICIPANTS={}
+      
 
