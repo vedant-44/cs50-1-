7ef946dd08d0781dfadfd5a4e7c3bff71e44643a
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-05-28 02:38:19 +0000

    Sun, May 28, 2023, 8:08 AM +05:30

--- froshims/app.py
+++ froshims/app.py
@@ -1,17 +1,21 @@
 from flask import Flask,render_template,request
 
 app=Flask(__name__)
+PARTICIPANTS={}
 
 @app.route("/")
 def index():
     return render_template('index.html')
 @app.route("/msg",methods=['POST'])
 def msg():
-
+    name=request.form.get("name")
+    sport=request.form.get("sport")
+    PARTICIPANTS[name]=sport
 
     return render_template('msg.html')
 @app.route("/participants")
 def participants():
-      PARTICIPANTS={}
-      
+    return render_template('participants.html')
+
+
 
