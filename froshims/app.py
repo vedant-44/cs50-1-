cdea7a8780c2ab6b52d56fa2206aa02a30f2bca0
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-05-28 14:28:30 +0000

    Sun, May 28, 2023, 7:58 PM +05:30

--- froshims/app.py
+++ froshims/app.py
@@ -2,17 +2,20 @@ from flask import Flask,render_template,request
 
 app=Flask(__name__)
 PARTICIPANTS={}
-
+SPORTS=['basketball','badminton','cricket']
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html',sports=SPORTS)
 @app.route("/msg",methods=['POST'])
 def msg():
     name=request.form.get("name")
     sport=request.form.get("sport")
-    if(name=='None' or sport=='none'):
+    if not name:
+        return render_template('msg1.html')
+    if sport not in SPORTS:
         return render_template('msg1.html')
     PARTICIPANTS[name]=sport
+
 
     return render_template('msg.html')
 @app.route("/participants")
