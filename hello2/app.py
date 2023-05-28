a6acac9b943a8e80f1d62520fd1c62dfa77ee583
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-05-28 01:51:23 +0000

    Sun, May 28, 2023, 7:21 AM +05:30

--- hello2/app.py
+++ hello2/app.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template,request
 
 app=Flask(__name__)
 
-route@app("/",methods=['GET','POST'])
+@app.route("/",methods=['GET','POST'])
 def index():
     if request.method=='GET':
         return render_template('index.html')
