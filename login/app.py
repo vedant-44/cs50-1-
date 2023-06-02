9345e3c01574f306ca6b7055aeb185a73c90fb11
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-06-02 14:13:43 +0000

    Fri, Jun 2, 2023, 7:43 PM +05:30

--- login/app.py
+++ login/app.py
@@ -1,3 +1,12 @@
 from flask import Flask,render_template,session,request
+from flask_session import Session
 
 app=Flask(__name__)
+
+app.config["SESSION_PERMANENT"]=False
+app.config["SESSION_TYPE"]="filesystem"
+Session(app)
+
+@app.route("/")
+def index():
+    
