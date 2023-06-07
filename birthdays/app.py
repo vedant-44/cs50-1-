22b51424acb2a9a7c2f8fc1d757f359db3e29901
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-06-07 03:18:22 +0000

    Wed, Jun 7, 2023, 8:48 AM +05:30

--- birthdays/app.py
+++ birthdays/app.py
@@ -26,8 +26,10 @@ def after_request(response):
 def index():
     if request.method == "POST":
 
-       
-
+       name=request.form.get('name')
+       month=request.form.get('month')
+       day=request.form.get('day')
+        
 
         return redirect("/")
 
