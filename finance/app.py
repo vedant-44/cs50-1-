0cbbf1aba1209683edb570c6bfb70085cc38287d
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-06-07 08:03:54 +0000

    Wed, Jun 7, 2023, 1:33 PM +05:30

--- finance/app.py
+++ finance/app.py
@@ -127,9 +127,9 @@ def register():
         db.execute("INSERT INTO users(username,hash) VALUES(?,?)",username1,hash_password)
 
         #log in user
-         rows=db.execute("SELECT * FROM users WHERE username=?",username1)
-         session["user_id"] = rows[0]["id"]
-         return redirect('/')
+        rows=db.execute("SELECT * FROM users WHERE username=?",username1)
+        session["user_id"] = rows[0]["id"]
+        return redirect('/')
     else:
         return render_template('register.html')
 
