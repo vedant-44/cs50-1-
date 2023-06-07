bfe5e25c1855b9ae392d0600da7b7f9c3b970eec
vedant-44 <110277442+vedant-44@users.noreply.github.com>
2023-06-07 03:35:33 +0000

    Wed, Jun 7, 2023, 9:05 AM +05:30

--- birthdays/app.py
+++ birthdays/app.py
@@ -33,13 +33,13 @@ def index():
          return redirect('/')
        else:
         db.execute("INSERT INTO birthdays(name,month,day) VALUES(?,?,?)",name,month,day )
-        
+
         return redirect("/")
 
     else:
 
         # TODO: Display the entries in the database on index.html
-
-        return render_template("index.html")
+        birthdays=db.execute("SELECT * FROM birthdays")
+        return render_template("index.html",birthdays=birthdays)
 
 
